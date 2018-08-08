fd1b5f77c0411853590c1e7f623dcddd53d1c68e
Ashley White <hello@ashleyd.ws>
2018-08-08 20:31:50 +0200

    Fix IE 11
    fixes #277

--- ekko-lightbox.js
+++ ekko-lightbox.js
@@ -650,7 +650,7 @@ const Lightbox = (($) => {
 			}
 
 			this._$lightboxContainer.css('height', maxHeight)
-			this._$modalDialog.css('flex', 1).css('maxWidth', width);
+			this._$modalDialog.css('flex', '1').css('maxWidth', width);
 
 			let modal = this._$modal.data('bs.modal');
 			if (modal) {
